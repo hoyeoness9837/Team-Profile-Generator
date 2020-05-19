0c999142788ced27535f12baa03ffbf3cea894b3
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-05-19 10:17:42 -0700

    changed test connecting path or javascript

--- Develop/test/Manager.test.js
+++ Develop/test/Manager.test.js
@@ -1,5 +1,5 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../manager.js");
+const Employee = require("../employee.js");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
